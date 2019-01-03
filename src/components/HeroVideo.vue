a21c6fe4158e666877b46b36eeaa4390590d056d
iamdeveloper <danlex@localhost.localdomain>
2019-01-03 21:37:13 +0100

    finished basic functionality of the app

---
+++ src/components/HeroVideo.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="videoWrapper">
+    <!-- <div class="overlay" /> -->
+    <video src="../assets/heroVideo.mp4" autoplay loop></video>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.videoWrapper {
+    .overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 1;
+        background-color: rgba(0,0,0,.4);
+
+    }
+
+    video {
+        position: fixed;
+        right: 0;
+        top: 0;
+        min-width: 100%;
+        min-height: 100%;
+    }
+}
+</style>
---
+++ src/components/Sumup.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="summaryWrapper">
+        <div class="summaryWrapper__items">
+            <h1>Your booking:</h1>
+            <p>City: {{form.city}}</p>
+            <p>From: {{form.from}}</p>
+            <p>To: {{form.to}}</p>
+            <p>Number of guests: {{form.selected}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        form: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .summaryWrapper {
+        position: relative;
+        z-index: 3;
+        height: 100%;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+
+
+
+        &__items {
+            padding: 20px;
+            background-color: rgba(0,0,0,.5);
+
+            h1 {
+                font-size: 44px;
+                text-align: center;
+                color: #fff;
+
+            }
+
+
+            & > p {
+                font-size: 40px;
+                color: #fff;
+            }
+        }
+    }
+</style>
